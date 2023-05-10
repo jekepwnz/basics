c5dffb33707b9b1862e9400337e30a83f0335305
Evgenii Volkov <hardcoreokje@gmail.com>
2023-05-10 17:01:39 +0300

    fixed task_06 exceptions

--- task_06.py
+++ task_06.py
@@ -1,11 +1,28 @@
-def rps_game_winner(turns):
+class WrongNumberOfPlayersError(Exception):
+    pass
+class NoSuchStrategyError(Exception):
+    pass
+
+
+def errors_check(turns):
     if len(turns) != 2:
-        raise Exception("WrongNumberOfPlayersError")
+        raise WrongNumberOfPlayersError()
     elif turns[0][1] not in "RPS" or turns[1][1] not in "RPS":
-        raise Exception("NoSuchStrategyError")
-    p2win = ["SR", "PS", "RP"]
-    if turns[0][1] + turns[1][1] in p2win:
-        return " ".join(turns[1])
+        raise NoSuchStrategyError()
+
+def rps_game_winner(turns):
+    try:
+        errors_check(turns)
+    except WrongNumberOfPlayersError:
+        return "WrongNumberOfPlayersError"
+    except NoSuchStrategyError:
+        return "NoSuchStrategyError"
+
     else:
-        return " ".join(turns[0])
+        p2win = ["SR", "PS", "RP"]
+        if turns[0][1] + turns[1][1] in p2win:
+            return " ".join(turns[1])
+        else:
+            return " ".join(turns[0])
+
 
